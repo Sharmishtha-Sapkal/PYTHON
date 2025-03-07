aa9e7496e4ef66cae18c91241e39da0157021b67
Sharmishtha Sapkal <sharusapkal16344@gmail.com>
2025-03-07 22:52:33 +0530

    Switch Case, loop and input practice

--- trail.py
+++ trail.py
@@ -1,2 +1,22 @@
-msg = "Hello python"
-print(msg);
+case = int(input("Enter Your Choice"))
+def python_switch(case):
+    
+    match case:
+        case 1:
+            n = 20
+            if(n>18):
+                print("You are an adult")
+            else:
+                print("You are not an adult")
+
+        case 2:
+            print("you are wishing to print factorial of a number")
+            num = int(input("Enter a number: "))
+            factorial = 1
+            for i in range(1, num + 1):
+                factorial *= i
+            print("Factorial of", num, "is", factorial)
+            
+        case _:
+            print("Enter valid choice")
+python_switch(case) 
