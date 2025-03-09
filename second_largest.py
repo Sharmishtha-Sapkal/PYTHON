a6c3dcd61f0d60edf015dab7b2652a615313a053
Sharmishtha-Sapkal <techheavensphere@gmail.com>
2025-03-09 21:08:36 +0530

    applied conditions for empty set and 1 value set

--- second_largest.py
+++ second_largest.py
@@ -1,13 +1,24 @@
 #Write a Python program that takes a list of numbers as input 
 # and returns the second largest number in the list.
 
-my_set = {10, 20, 30}
-maximum = max(my_set)
-print("first max value :", maximum)
-my_set.remove(maximum)
+my_set = {}
 
-#print(my_set)
-second_max = max(my_set)
-print("second max value :", second_max)
+
+if(len(my_set)==0):
+    print("set is empty")
+
+elif((my_set)<2):
+    print("their is no second value")
+    
+else:
+    maximum = max(my_set)
+    print("first max value :", maximum)
+    my_set.remove(maximum)
+
+    #print(my_set)
+    second_max = max(my_set)
+    print("second max value :", second_max)
+
+
 
     
