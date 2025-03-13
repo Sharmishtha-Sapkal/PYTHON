23b9dd0181896ac5308f44ed2c3115773cb58929
Sharmishtha-Sapkal <techheavensphere@gmail.com>
2025-03-13 23:37:41 +0530

    odd and prime done

---
+++ Python_day_2.py
@@ -0,0 +1,24 @@
+#list of all odd numbers
+
+for i in range (1,101,2):
+    print (i)
+
+#prime number code in range 1-100
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+primes = [num for num in range(1, 101) if is_prime(num)]
+print("Prime numbers between 1 and 100:", primes)
+
+#swap last two digits 
+def swap_last_two_digits(num):
+    last_two_digits = num % 100
+    first_two_digits = num // 100
+    swapped_num = (first_two_digits * 100) + (last_two_digits % 10)
+    return swapped_num
