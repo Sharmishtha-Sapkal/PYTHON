729e412828eaaf7c34c5f9d6f7d088bcc7968de1
Sharmishtha-Sapkal <techheavensphere@gmail.com>
2025-03-26 23:02:28 +0530

    dessert_item class and constructor created

---
+++ desser_miniproject.py
@@ -0,0 +1,17 @@
+from abc import abstractmethod
+
+
+from abc import ABC  
+
+class dessert_item(ABC):
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    @abstractmethod
+    def get_cost(self):
+        pass
+
