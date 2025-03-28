c46cf4ad215b9670b45483ca0461d14ef545b95a
Sharmishtha-Sapkal <techheavensphere@gmail.com>
2025-03-28 22:57:56 +0530

    pattern

--- py_assi_2.py
+++ py_assi_2.py
@@ -11,7 +11,7 @@ print("Sum:", add_numbers([10, 20, 30, 40, 50]))
 def performAction(lst, n):
     print("in perform action")
     new_list = []  # Initialize new_list to store elements
-    for i in range(len(lst)):
+    for i in range(len(lst)): 
         new_list.append(lst[i])  # Append the current element to new_list
         if (i + 1) % n == 0:  # Check if the current index + 1 is a multiple of n
             print("New list after adding", lst[i], ":", new_list)  # Print the current state of new_list
@@ -20,7 +20,7 @@ def performAction(lst, n):
         
         if i == n:  # Check if the current element is equal to n
             second_list.append(i)  # Append to second_list if it matches
-    print("Occurrences of", n, "in the list:", second_list)  # Print the second_list
+            print("Occurrences of", n, "in the list:", second_list)  # Print the second_list
 
 
         # if n in lst:
